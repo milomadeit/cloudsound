7ff536b9af672b5845c60ab06eca3825da9ef58f
Zach Milo <zachmilo@gmail.com>
2024-01-11 18:43:25 -0500

    table relationships

--- app/models/comment.py
+++ app/models/comment.py
@@ -8,7 +8,7 @@ class Comment(db.Model):
         __table_args__ = {'schema': SCHEMA}
         
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     content = db.Column(db.String(100), nullable=False)
 
     user = db.relationship('User', back_populates='comments')
