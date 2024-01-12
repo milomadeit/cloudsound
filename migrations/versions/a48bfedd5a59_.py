b2a77cb9401f484a4411e3f5290e7ee82cc306b8
Zach Milo <zachmilo@gmail.com>
2024-01-11 21:01:27 -0500

    tsting

--- migrations/versions/a48bfedd5a59_.py
+++ migrations/versions/a48bfedd5a59_.py
@@ -7,6 +7,9 @@ Create Date: 2024-01-11 20:38:56.393468
 """
 from alembic import op
 import sqlalchemy as sa
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get("SCHEMA")
 
 
 # revision identifiers, used by Alembic.
@@ -67,6 +70,8 @@ def upgrade():
     sa.PrimaryKeyConstraint('song_id', 'playlist_id')
     )
     # ### end Alembic commands ###
+    if environment == "production":
+        op.execute(f"ALTER TABLE <table_name> SET SCHEMA {SCHEMA};")
 
 
 def downgrade():
