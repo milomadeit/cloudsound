c9bb51d1db8140cce12abd543c2bf8f37193373c
Zach Milo <zachmilo@gmail.com>
2024-01-11 20:15:48 -0500

    db testing

--- app/models/playlist.py
+++ app/models/playlist.py
@@ -1,4 +1,5 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
+from .user import User
 
 class Playlist(db.Model):
 	__tablename__ = 'playlists'
@@ -8,6 +9,7 @@ class Playlist(db.Model):
 
 	id = db.Column(db.Integer, primary_key=True)
 	title = db.Column(db.String(60), nullable=False)
+	user_id = db.Column(db.Integer, db.ForeignKey(User.id))
 
 	user = db.relationship('User', back_populates='playlists')
 	songs = db.relationship('Song', secondary="playlist_songs", back_populates='playlists')
