d6d076d3d8753504a1d420e4694ad11080da3e06
Zach Milo <zachmilo@gmail.com>
2024-01-11 19:01:40 -0500

    db tester

--- app/__init__.py
+++ app/__init__.py
@@ -4,12 +4,11 @@ from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect, generate_csrf
 from flask_login import LoginManager
-from .models import db, User
+from .models import db, User, Song, Comment, Like, Playlist, playlists_song
 from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
 from .seeds import seed_commands
 from .config import Config
-from .models import User, Song, Comment, Like, Playlist, playlists_song
 
 app = Flask(__name__, static_folder='../react-app/build', static_url_path='/')
 
