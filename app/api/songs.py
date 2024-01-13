9b33702d78069fddd5763ca50c3d60e720b4dafb
Zach Milo <zachmilo@gmail.com>
2024-01-13 14:50:22 -0500

    aws setup

---
+++ app/api/songs.py
@@ -0,0 +1,17 @@
+from flask import Blueprint, jsonify
+from ..forms.song_validation_form import SongForm
+from .s3buckets import get_unique_filename, upload_file_to_s3, remove_file_from_s3
+
+song_routes = Blueprint('songs', __name__)
+
+@song_routes.route('/upload/', methods=['POST'])
+def SongUpload():
+	form = SongForm();
+
+	if form.validate_on_submit():
+		print(form.data)
+
+	return 'Did we print?'
+
+
+	
---
+++ app/forms/song_validation_form.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed, FileRequired, DataRequired
+from wtforms import SubmitField, StringField
+from ..api.s3buckets import ALLOWED_EXTENSIONS
+
+class SongForm(FlaskForm):
+    song = FileField("Song File", validators=[FileRequired(), FileAllowed(list(ALLOWED_EXTENSIONS))])
+    title = StringField('Title', validators=[DataRequired()])
+    submit = SubmitField("Create Post")
