7bc00a29e6d61e3db767e92eea030071f25e9988
Zach Milo <zachmilo@gmail.com>
2024-01-12 23:31:59 -0500

    db testing

--- app/models/song.py
+++ app/models/song.py
@@ -9,7 +9,7 @@ class Song(db.Model):
 		__table_args__ = {'schema': SCHEMA}
 
 	id = db.Column(db.Integer(), primary_key=True)
-	user_id = db.Column(db.Integer, db.ForeignKey('users.id'))  # references users id column
+	user_id = db.Column(db.Integer, db.ForeignKey(User.id))  # references users id column
 	title = db.Column(db.String(50), nullable=False)
 	artist = db.Column(db.String(50), nullable=False)
 	genre = db.Column(db.String())
