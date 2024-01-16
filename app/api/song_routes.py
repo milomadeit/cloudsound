cb345d22f292c3a3cf6cd4f8380c8150631d3903
Zach Milo <zachmilo@gmail.com>
2024-01-16 18:12:36 -0500

    dev branch

--- app/api/song_routes.py
+++ app/api/song_routes.py
@@ -4,6 +4,7 @@ from .s3buckets import get_unique_filename, upload_file_to_s3, remove_file_from_
 
 song_routes = Blueprint('songs', __name__)
 
+# on dev branch
 @song_routes.route('/upload/', methods=['POST'])
 def SongUpload():
 	form = SongForm();
