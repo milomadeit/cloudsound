95736bfafa5f4a6590e65078a647dcaf8f3a6450
Visnja Vracaric <visnja.vracaric@gmail.com>
2024-01-17 17:53:22 -0500

    api routes/comments/beginning

--- app/api/comments.py
+++ app/api/comments.py
@@ -1,4 +1,8 @@
 from flask import Blueprint
+from app.forms.comment_validation_form import CommentForm
+from app.models.comment import Comment
+from app.models.db import db
+from flask_login import current_user
 
 comments_bp = Blueprint('comments_bp', __name__)
 
@@ -6,3 +10,22 @@ comments_bp = Blueprint('comments_bp', __name__)
 @comments_bp.route('/comment-test')
 def tester():
   return 'hello from comment-test route'
+
+@comments_bp.route("/songs/<int:id>/comments")
+def get_comment_form():
+  form=CommentForm()
+  return
+
+@comments_bp.route("/songs/<int:id>/comments", methods=['POST'])
+def post_comment(id):
+  form=CommentForm()
+  if current_user:
+      if form.validate_on_submit():
+          comment=form.comment.data
+
+          new_comment=Comment(user_id=current_user.id,song_id={id},comment=comment)
+
+          db.session.add(new_comment)
+          db.session.commit()
+          return redirect("/")
+  return "Bad Data"
---
+++ app/forms/comment_validation_form.py
@@ -0,0 +1,8 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import DataRequired
+from wtforms import SubmitField, StringField
+
+
+class CommentForm(FlaskForm):
+    comment = StringField('Comment', validators=[DataRequired()])
+    submit = SubmitField("Create Comment")
