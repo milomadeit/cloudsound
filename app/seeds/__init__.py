6e1f3db38b4c35c9b897c55c5f355c3c0b82b4c3
Visnja Vracaric <visnja.vracaric@gmail.com>
2024-01-18 18:22:45 -0500

    adding new seeds

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -13,13 +13,15 @@ seed_commands = AppGroup('seed')
 @seed_commands.command('all')
 def seed():
     if environment == 'production':
-        # Before seeding in production, you want to run the seed undo 
-        # command, which will  truncate all tables prefixed with 
+        # Before seeding in production, you want to run the seed undo
+        # command, which will  truncate all tables prefixed with
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
         db.session.execute(f"TRUNCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
+        db.session.execute(f"TRUNCATE table {SCHEMA}.songs RESTART IDENTITY CASCADE;")
         db.session.commit()
         undo_users()
+        undo_songs()
 
     seed_users()
     seed_songs()
--- app/seeds/songs.py
+++ app/seeds/songs.py
@@ -23,8 +23,8 @@ def seed_songs():
 # it will reset the primary keys for you as well.
 def undo_songs():
     if environment == "production":
-        db.session.execute(f"TRUNCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
+        db.session.execute(f"TRUNCATE table {SCHEMA}.songs RESTART IDENTITY CASCADE;")
     else:
-        db.session.execute(text("DELETE FROM users"))
-        
+        db.session.execute(text("DELETE FROM songs"))
+
     db.session.commit()
