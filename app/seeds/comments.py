d44e28cd33f796f226ec2c0cf8971b7899b152c6
Visnja Vracaric <visnja.vracaric@gmail.com>
2024-01-17 19:20:03 -0500

    comments seeds

--- app/seeds/comments.py
+++ app/seeds/comments.py
@@ -1,4 +1,4 @@
-from app.models import db, User, environment, SCHEMA, Comment
+from app.models import db, Comment, environment, SCHEMA,
 from sqlalchemy.sql import text
 
 
@@ -7,30 +7,27 @@ def seed_comments():
     comment1 = Comment(
         user_id=3, song_id=1, content='Love the song!!')
     comment2 = Comment(
-        user_id=2, song_id=1, content='Not my cup of tea')
+        user_id=2, song_id=2, content='Not my cup of tea')
     comment3 = Comment(
-        user_id=1, song_id= content='Too slow for me')
+        user_id=1, song_id=3 content='Too slow for me')
     comment4 = Comment(
-        user_id=, content='Too slow for me')
+        user_id=1, song_id=1,content='My wedding song <3')
     comment5 = Comment(
-        user_id=1, content='Too slow for me')
+        user_id=1,song_id=2 content='Fire')
 
     db.session.add(comment1)
     db.session.add(comment2)
     db.session.add(comment3)
+    db.session.add(comment4)
+    db.session.add(comment5)
     db.session.commit()
 
 
-# Uses a raw SQL query to TRUNCATE or DELETE the users table. SQLAlchemy doesn't
-# have a built in function to do this. With postgres in production TRUNCATE
-# removes all the data from the table, and RESET IDENTITY resets the auto
-# incrementing primary key, CASCADE deletes any dependent entities.  With
-# sqlite3 in development you need to instead use DELETE to remove all data and
-# it will reset the primary keys for you as well.
-def undo_users():
+
+def undo_comments():
     if environment == "production":
-        db.session.execute(f"TRUNCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
+        db.session.execute(f"TRUNCATE table {SCHEMA}.comments RESTART IDENTITY CASCADE;")
     else:
-        db.session.execute(text("DELETE FROM users"))
+        db.session.execute(text("DELETE FROM comments"))
 
     db.session.commit()
