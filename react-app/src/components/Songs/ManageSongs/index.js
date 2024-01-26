57024b6bc65dadf884f2e12065e3c95590def9a3
Shreya <shreya.r.darekar@gmail.com>
2024-01-25 21:32:38 -0800

    use GenreSongs for manageSongs

--- react-app/src/components/Songs/ManageSongs/index.js
+++ react-app/src/components/Songs/ManageSongs/index.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import { getCurrentUserSongs } from "../../../store/songs";
-import SongBox from "../../SongBox";
+import GenreSongs from "../../GenreSongs";
 
 const ManageSongs = () => {
   const history = useHistory();
@@ -15,38 +15,18 @@ const ManageSongs = () => {
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-
     if (!user) {
-      history.push('/');
-      return; 
+      history.push("/");
+      return;
     }
     dispatch(getCurrentUserSongs()).then(() => setLoading(false));
-  }, [dispatch, user, history ]);
-
+  }, [dispatch, user, history]);
 
   if (loading) return <h1>Loading...</h1>;
 
-
   return (
-    <div>
-      <div>
-        <h2>Recent Uploads</h2>
-        {songs.map((song) => (
-          <SongBox 
-          key={song.id}
-          id={song.id}
-          artist={song.artist}
-          title={song.title}
-          genre={song.genre}
-          image_url={song.image_url}
-          play_count={song.play_count}
-          likes={song.likes}
-          song_url={song.song_url}
-          user_id={song.user_id}
-          />
-        ))}
-      </div>
-
+    <div style={{ padding: "20px" }}>
+      <GenreSongs genre="My Uploads" songs={songs} />
     </div>
   );
 };
