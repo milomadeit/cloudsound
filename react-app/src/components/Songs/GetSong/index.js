37b2a46e06ca46f60786d8687ed445decca24ccd
Visnja Vracaric <visnja.vracaric@gmail.com>
2024-01-25 19:01:31 -0500

    qwe

--- react-app/src/components/Songs/GetSong/index.js
+++ react-app/src/components/Songs/GetSong/index.js
@@ -13,6 +13,7 @@ import image1 from "./logo/heart.png"
 import image2 from "./logo/add-to-playlist-3.png"
 import image3 from "./logo/delete-2.png"
 import image4 from "./logo/edit.png"
+import image5 from "./logo/chatting.png"
 
 
 import * as commentActions from '../../../store/comments'
@@ -25,7 +26,7 @@ const GetSong = () => {
 
   const comments = Object.values(useSelector((state) => state.comments));
   const sessionUser = useSelector((state) => state.session.user);
-
+console.log(comments)
   const song = useSelector((state) => state.songsReducer.allSongs[parseInt(songId)]);
 
 
@@ -50,23 +51,22 @@ const GetSong = () => {
 
 
   return (
-    <div>
-      <h1>{song.title}</h1>
-      <h2>{song.artist}</h2>
+    <div className="container-for-song-details">
+
 
             <CreateSongComment />
             <span> <NavLink exact to="/"><img id="logo1"src={image1} style={{width:"20px", height:"20px"}}/>Like</NavLink></span>
             <span> <NavLink exact to="/"><img id="logo2"src={image2} style={{width:"20px", height:"20px"}}/></NavLink></span>
-
+<div clasName="song-details-info">
       <h3>#{song.genre}</h3>
       <h3>{song.title}</h3>
       <h3>{song.artist}</h3>
       <h3>{song.likes}</h3>
+</div>
 
 
-
-      <div>
-      <h3>Comments:</h3>
+      <div className="song-details-all-comments">
+      <span><img id="logo5"src={image5} style={{width:"20px", height:"20px"}}/><h3>{comments.length} comments</h3></span>
     {isLoaded &&(
       comments.map((comment) =>
 
