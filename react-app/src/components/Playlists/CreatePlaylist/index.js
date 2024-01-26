0045ca796660827969b3753b45a94a7aa6e7a6f1
Tayvor <tabeck98@gmail.com>
2024-01-26 16:52:36 -0700

    No longer able to create a playlist with the same title, under the same user.

--- react-app/src/components/Playlists/CreatePlaylist/index.js
+++ react-app/src/components/Playlists/CreatePlaylist/index.js
@@ -8,19 +8,36 @@ const CreatePlaylist = () => {
   const dispatch = useDispatch();
   const history = useHistory();
   const [title, setTitle] = useState('');
+  const [errors, setErrors] = useState('');
 
   const userId = useSelector((state) => state.session.user.id)
+  const playlists = Object.values(useSelector((state) => state.playlists))
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const formData = new FormData();
-    formData.append('title', title);
-    formData.append('user_id', userId);
 
-    dispatch(playlistActions.create_playlist_thunk(formData));
-    dispatch(playlistActions.get_playlists_thunk());
-    history.push('/playlists')
+    if (title.length && checkPlaylistNames(title)) {
+      const formData = new FormData();
+      formData.append('title', title);
+      formData.append('user_id', userId);
+
+      dispatch(playlistActions.create_playlist_thunk(formData));
+      dispatch(playlistActions.get_playlists_thunk());
+      history.push('/playlists')
+    }
   }
+
+  const checkPlaylistNames = (title) => {
+    setErrors('');
+
+    for (let pl of playlists) {
+      if (pl.title === title) {
+        setErrors('A Playlist with that title already exists.')
+        return false;
+      }
+    }
+    return true;
+  };
 
   return (
     <form onSubmit={handleSubmit}>
@@ -35,7 +52,12 @@ const CreatePlaylist = () => {
         </label>
       </div>
 
-      <button type="submit">Create Playlist!</button>
+      {errors && <p>{errors}</p>}
+
+      <button
+        type="submit"
+        disabled={title.length ? false : true}
+      >Create Playlist!</button>
     </form>
   )
 }
