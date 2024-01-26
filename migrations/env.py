6b767a018e34b801888d24a6f8591a01e5a4e024
Zach Milo <zachmilo@gmail.com>
2024-01-25 20:02:11 -0500

    savinggg

--- migrations/env.py
+++ migrations/env.py
@@ -98,8 +98,4 @@ def run_migrations_online():
 if context.is_offline_mode():
     run_migrations_offline()
 else:
-<<<<<<< HEAD
     run_migrations_online()
-=======
-    run_migrations_online()
->>>>>>> get-song
