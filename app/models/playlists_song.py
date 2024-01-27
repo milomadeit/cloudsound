7f86d8cc9c7e99ebd8b4c6e5c18c6d9f094e4d13
Zach Milo <zachmilo@gmail.com>
2024-01-27 18:23:26 -0500

    debug

--- app/models/playlists_song.py
+++ app/models/playlists_song.py
@@ -13,5 +13,6 @@ if environment == "production":
 # Should handle many to many relationship and create a join table
 playlistsongs = db.Table(
     __tablename__,
+    db.metadata,
     db.Column("song_id", db.ForeignKey(Song.id), primary_key=True),
     db.Column("playlist_id", db.ForeignKey(Playlist.id), primary_key=True))
