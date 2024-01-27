b8baaf82c1059f7f9edd324e808b4666229c5fb6
Tayvor <tabeck98@gmail.com>
2024-01-27 13:32:44 -0700

    redirect to playlist when adding a song

--- react-app/src/components/PlaylistSongs/AddPlaylistSong/index.js
+++ react-app/src/components/PlaylistSongs/AddPlaylistSong/index.js
@@ -1,17 +1,18 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import * as plSongActions from '../../../store/playlist-songs'
 import { getAllSongs } from "../../../store/songs";
 
 
 const AddSongToPL = () => {
   const dispatch = useDispatch();
+  const history = useHistory();
   const { songId } = useParams();
 
   const [playlistId, setPlaylistId] = useState(0)
 
-  
+
   const playlists = Object.values(useSelector((state) => state.playlists))
   const songDetails = useSelector((state) => state.songsReducer.allSongs[songId]);
   const song = songDetails ? songDetails.title : '';
@@ -19,11 +20,9 @@ const AddSongToPL = () => {
 
 
   useEffect(() => {
-
     dispatch(getAllSongs())
-    
   }, [dispatch]);
-  
+
   const addSongToPlaylist = (e) => {
     e.preventDefault()
     if (playlistId) {
@@ -32,6 +31,7 @@ const AddSongToPL = () => {
       formData.append('songId', songId);
 
       dispatch(plSongActions.add_song_to_pl(formData, playlistId))
+        .then(() => history.push(`/playlists/${playlistId}`))
     }
   }
 
@@ -41,6 +41,7 @@ const AddSongToPL = () => {
       {songDetails ? (
         <>
           <h3>Adding '{song}' by '{artist}' to: </h3>
+
           <form onSubmit={addSongToPlaylist}>
             <select onChange={(e) => setPlaylistId(e.target.value)}>
               <option key={0} value={''}>-- Select A Playlist --</option>
@@ -56,7 +57,7 @@ const AddSongToPL = () => {
       )}
     </>
   );
-  
+
 }
 
 export default AddSongToPL;
